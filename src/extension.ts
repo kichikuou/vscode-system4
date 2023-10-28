1cefef5e649568eadb03dc7e939bfeafacc758ec
kichikuou <KichikuouChrome@gmail.com>
2023-10-28 11:16:35 +0900

    Find .ain file and pass its path to system4-lsp

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import * as fs from 'fs';
+import * as path from 'path';
 import * as vscode from 'vscode';
 import * as languageclient from 'vscode-languageclient/node';
 
@@ -22,9 +23,11 @@ export async function deactivate() {
 }
 
 async function startClient() {
+    const lspPath = getLspPath();
+    const ainPath = await findAin();
     const serverOptions = {
-        command: await getLspPath(),
-        args: []
+        command: await lspPath,
+        args: ainPath ? ['--ain', ainPath] : [],
     };
     const clientOptions = {
         documentSelector: [
@@ -67,4 +70,47 @@ async function getLspPath(): Promise<string> {
 
     // Otherwise, return "system4-lsp" hoping it is in PATH.
     return exeName;
+}
+
+async function findAin(): Promise<string | undefined> {
+    const wsPath = vscode.workspace.workspaceFolders?.[0]?.uri.fsPath;
+    // Determine the path of the AIN file to be read by several heuristics.
+    const possibleDirs = [
+        // Workspace root
+        Promise.resolve(wsPath),
+        // Parent directory of the workspace root
+        Promise.resolve(wsPath && path.dirname(wsPath)),
+        // `OutputDir` directory of .pje file in the workspace root
+        getProjectOutputDir(),
+    ];
+    for await (const dir of possibleDirs) {
+        if (!dir) continue;
+        try {
+            // Return the ain path written in AliceStart.ini or System40.ini.
+            let ini = await Promise.any([
+                readSjisFile(`${dir}/AliceStart.ini`),
+                readSjisFile(`${dir}/System40.ini`),
+            ]);
+            const match = ini.match(/^CodeName\s*=\s*"(.*)"\s*$/m);
+            return match ? `${dir}/${match[1]}` : undefined;
+        } catch (_) {}
+    }
+    return undefined;
+}
+
+// Searches for the first file with a `.pje` extension in the current workspace
+// and returns the directory path specified in the `OutputDir` field of the file.
+async function getProjectOutputDir(): Promise<string | undefined> {
+    const pjeFiles = await vscode.workspace.findFiles('*.pje');
+    if (pjeFiles.length === 0) return undefined;
+    const pjePath = pjeFiles[0].fsPath;
+    const pje = await readSjisFile(pjePath);
+    const match = pje.match(/^OutputDir\s*=\s*"(.*)"\s*$/m);
+    if (!match) return undefined;
+    return path.join(path.dirname(pjePath), match[1]);
+}
+
+async function readSjisFile(path: string): Promise<string> {
+    const content = await fs.promises.readFile(path);
+    return new TextDecoder('shift_jis').decode(content);
 }
